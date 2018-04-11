2822782675a057659f108c891f0bd1809f26310b
Hassan <hassanhoumaid92@gmail.com>
2018-04-11 08:17:42 +0100

    Auth component added

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,11 +3,13 @@ import NavigationItem from './NavigationItem/NavigationItem';
 import classes from './NavigationItems.css';
 
 const navigationItems = () => (
-	<ul className = {classes.NavigationItems}>
-		<NavigationItem link="/" exact>Burger Builder</NavigationItem>
+	<ul className={classes.NavigationItems}>
+		<NavigationItem link="/" exact>
+			Burger Builder
+		</NavigationItem>
 		<NavigationItem link="/orders">Orders</NavigationItem>
+		<NavigationItem link="/auth">Authenticate</NavigationItem>
 	</ul>
-
 );
 
 export default navigationItems;
